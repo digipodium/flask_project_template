3d4e0f146b277182b647dcb4194964e4a36bd096
zaid kamil <xaidmetamorphos@gmail.com>
2020-03-09 14:52:09 +0530

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-have-to-guess'
+app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,30 @@
+from flask import render_template,redirect,request,flash,session,url_for
+from flask_login import logout_user,current_user, login_user
 from app import app
-from flask import render_template,redirect
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html',title='home')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username & password:
+            user = User.query.filter_by(username=username).first()
+            if user is None or not user.check_password(password):
+                flash('Invalid username or password')
+                return redirect(url_for('login'))
+            login_user(user, remember=True)
+            return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In')
     
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
